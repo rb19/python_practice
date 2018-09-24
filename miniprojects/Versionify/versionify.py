e1e3eb256fb13b6ebe3527deef81ffbdc1aa368e
Raymon Baek <raymon@gotruemotion.com>
2018-09-24 09:49:22 -0400

    Update directory to local git repo

--- miniprojects/Versionify/versionify.py
+++ miniprojects/Versionify/versionify.py
@@ -16,7 +16,7 @@ import os
 
 current_dir = os.getcwd()
 # Specify the directory to the file you want to overwrite
-file_dir = "/Desktop/Versionify"
+file_dir = "/personal_repos/python_practice/Versionify"
 new_dir = current_dir+file_dir
 #Specify the file name that you want to overwrite
 file_name = "versions.json"
