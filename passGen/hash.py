65b378808b7f29a93b8573baf03d43b52c262dea
Raymon Baek <raymon@gotruemotion.com>
2018-10-01 22:50:49 -0400

    Let user enter length of password.

--- passGen/hash.py
+++ passGen/hash.py
@@ -3,8 +3,10 @@ import random
 def passGen():
 	#print random.randint(1,4)
 	myList = []
+	x = input("Enter number of characters you want for your generated password: ")
+
 	for i in range (10):
-		myList.append(str(random.randint(0,9)))
+		myList.append(str(random.randint(0,x)))
 	joinList = ''.join(myList)
 	print "Your new generated password: " + joinList
 
