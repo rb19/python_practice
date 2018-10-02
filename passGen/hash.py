66f532da97bebaa5d932813c46656792a972ed4a
rb19 <raymon.baek@gmail.com>
2018-10-02 07:56:42 -0400

    Fix setting password length

--- passGen/hash.py
+++ passGen/hash.py
@@ -3,10 +3,15 @@ import random
 def passGen():
 	#print random.randint(1,4)
 	myList = []
-	x = input("Enter number of characters you want for your generated password: ")
+	passLength = input("Enter number of characters you want for your generated password: ")
+	# TODO: Add checks for each category.
+	# num_check = input("Enable numbers? (Y/N): ")
+	# upper_check = input("Enable uppercase letters? (Y/N): ")
+	# lower_check = input("Enable lowercase letters? (Y/N): ")
+	# special_check = input("Enable special characters (ie !@#$%^&*)? (Y/N): ")
 
-	for i in range (10):
-		myList.append(str(random.randint(0,x)))
+	for i in range (passLength):
+		myList.append(str(random.randint(0,9)))
 	joinList = ''.join(myList)
 	print "Your new generated password: " + joinList
 
